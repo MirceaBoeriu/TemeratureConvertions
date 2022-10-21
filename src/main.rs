0d41699a855dd4dc348a1d7ad4e475d2480476fa
MirceaBoeriu <mboeriu@pentalog.com>
2022-10-21 08:38:43 +0300

    more elegant

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-fn CF_converstions(c:f32)->f32{
+fn CF_converstions(c:f64)->f64{
     return (c*1.8)+32.0;
    }
-fn FC_converstions(f:f32)->f32{
+fn FC_converstions(f:f64)->f64{
     return (f-32.0)*(5.0/9.0);
 }
 fn main() {
-    println!("celcius to F for 30 = {}", CF_converstions(30.0));
-    println!("Fahrenheit to c for 86 = {}", FC_converstions(86.0));
+    let c=31.0;
+    let f= CF_converstions(c);
+    println!("Celsius to Fahrenheit for 30 = {}", CF_converstions(c));
+    println!("Fahrenheit to Celsius for 86 = {}", FC_converstions(f));
 }
